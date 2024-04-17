655058a1a81d1b78384df552ab841268880f5d5c
Eduardo Fiss Beloni <eduardo.fissbeloni@nttdata.com>
2024-04-17 12:13:07 -0300

    Initial commit - A new saga begins...

---
+++ src/main/java/com/example/swsaga/SwSagaApplication.java
@@ -0,0 +1,13 @@
+package com.example.swsaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwSagaApplication.class, args);
+	}
+
+}
