53c17f0588a46eaa34dfc812973c45f6ebeae885
Eduardo Fiss Beloni <eduardo.fissbeloni@nttdata.com>
2024-04-19 13:22:34 -0300

    Update unit tests to cover previous changes

--- src/test/java/com/example/swsaga/usecase/impl/SwSagaUseCaseImplTest.java
+++ src/test/java/com/example/swsaga/usecase/impl/SwSagaUseCaseImplTest.java
@@ -4,22 +4,45 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.swsaga.domain.SwApiFilm;
+import com.example.swsaga.domain.SwApiResponse;
+import com.example.swsaga.external.client.SwApiClient;
 
 @ExtendWith(MockitoExtension.class)
 public class SwSagaUseCaseImplTest {
 
-    @InjectMocks
     private SwSagaUseCaseImpl useCase;
 
+    @Mock
+    private SwApiClient client;
+
+    @Mock
+    private SwApiResponse apiResponse;
+
+    @Mock
+    private SwApiFilm film;
+
+    private List<SwApiFilm> films;
     private List<Integer> episodes = List.of(1, 2, 3, 4, 5, 6, 999);
     private final String DESCRIPTION = "Save the King";
+
+    @BeforeEach
+    void setup() {
+        films = List.of(film);
+        when(client.getFilms()).thenReturn(apiResponse);
+        when(apiResponse.getResults()).thenReturn(films);
+        useCase = new SwSagaUseCaseImpl(client);
+    }
 
     @Test
     void listFilmsTest() {
@@ -30,6 +53,7 @@ public class SwSagaUseCaseImplTest {
     @Test
     void filmDetailsTest() {
         episodes.forEach(ep -> {
+            if (ep <= 6) when(film.getEpisode()).thenReturn(ep);
             final var filmOpt = assertDoesNotThrow(() -> useCase.filmDetails(ep));
             if (ep > 6) {
                 assertTrue(filmOpt.isEmpty());
@@ -41,7 +65,10 @@ public class SwSagaUseCaseImplTest {
 
     @Test
     void updateFilmTest() {
+        when(film.getVersion()).thenReturn(2);
+        when(film.getDescription()).thenReturn(DESCRIPTION);
         episodes.forEach(ep -> {
+            if (ep <= 6) when(film.getEpisode()).thenReturn(ep);
             assertDoesNotThrow(() -> useCase.updateFilm(ep, DESCRIPTION));
             if (ep <= 6) {
                 final String description = useCase.filmDetails(ep).get().getDescription();
